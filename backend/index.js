8e38bac9c0c7b48b0e773185381a29a598023436
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-07-15 17:57:44 +0530

    database connected

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT=process.env.PORT
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,})
+.then(()=>console.log("database connected successfully"))
+.catch(err=>console.log(err))
+
+app.listen(PORT, ()=>{
+  console.log(`Server connected successfully on ${PORT}`);
+})
