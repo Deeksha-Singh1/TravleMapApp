3bcec77ea3dbc225b5dd1238b34ed09b6340f594
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-07-15 19:31:14 +0530

    pins route and model

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
-const PORT=process.env.PORT
-const app = express();
 
-app.use(express.json);
+const PORT = process.env.PORT || 8000;
+
+const pinRoutes = require('./routes/pins');
+
+dotenv.config();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,})
 .then(()=>console.log("database connected successfully"))
 .catch(err=>console.log(err))
+
+app.use('/api/pins',pinRoutes)
 
 app.listen(PORT, ()=>{
   console.log(`Server connected successfully on ${PORT}`);
